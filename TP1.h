92ff41f1b1a703927dc4f065cb9293e078b7acb7
Stefano Zdasien <Styzdasien@gmail.com>
2022-05-16 17:18:03 -0300

    ejercicio 1

---
+++ TP1.h
@@ -0,0 +1,34 @@
+#ifndef TP1_H_INCLUDED
+#define TP1_H_INCLUDED
+
+// EJERCICIO 1
+
+float superficieCirculo(int radio)
+
+{
+    return (3.1416 * radio * radio);
+}
+
+// EJERCICIO 2
+
+long suma(int desde, int hasta)
+{
+    for(i =desde; i <= hasta; i++)
+
+
+}
+
+
+//EJERCICIO 9
+
+/*
+void TablaDel(int n, int * Tabla)
+
+{
+    for(int i=0; i<10 ; ++i)
+      Tabla{i} = n * (i+1);
+}
+
+*/
+
+#endif // TP1_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include "TP1.h"
 
 using namespace std;
 
+//EJERCICIO 1
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Superficie Circunferencia Trigonometrica = ";
+    cout << superficieCirculo(1) << endl;
     return 0;
+
+    long potencia(int a, int b)
+
+    cout << "ingrese el denominador";
+    cin  >> a;
+    cout << "ingrese la potencia";
+    cin  >> b;
+    cout << "la respuesta es: " << a + b;
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
