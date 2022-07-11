93badc14c829955ceb20b425db1aba832557010e
Stefano Zdasien <styzdasien@gmail.com>
2022-07-10 22:08:13 -0300

    Paraentregar

--- TP1.h
+++ TP1.h
@@ -1,34 +1,245 @@
 #ifndef TP1_H_INCLUDED
 #define TP1_H_INCLUDED
+#endif
 
-// EJERCICIO 1
+using std::string;
 
+
+
+
+//EJERCICIO 1
 float superficieCirculo(int radio)
-
 {
-    return (3.1416 * radio * radio);
+    return(3.1416 * radio * radio);
 }
 
-// EJERCICIO 2
-
+//EJERCICIO 2
 long suma(int desde, int hasta)
 {
-    for(i =desde; i <= hasta; i++)
+    return (((hasta - desde + 1)*(desde + hasta)) /2 );
+}
 
+//EJERCICIO 3
+long potencia(int a, int b)
+{
+    long pot = 1;
+
+    for(int i = 1; i <= b; i++)
+         pot = pot * a;
+
+
+    return pot;
+}
+
+
+//EJERCICIO 4
+
+long factorial(int numero)
+    {
+        int i, fact = 1;
+        for(i = 1  ; i <=numero; i++)
+            fact = fact * i;
+
+         return fact;
+    }
+
+//EJERCICIO 5
+
+int edad(int dia, int mes, int anio)
+{
+    int anioActual = 2022, mesActual = 06, diaActual = 06;
+    int aux = anioActual - anio;
+
+    if(mes > mesActual)
+        aux --;
+    else
+        if((mesActual == mes) && (dia > diaActual))
+            aux;
+   return aux;
 
 }
 
+
+
+
+//EJERCICIO 6
+string romanos(int numero)
+   {
+       string numeroRomano;
+       int millares, centenas, decenas, unidades;
+
+       unidades = numero % 10; numero /= 10;
+       decenas = numero % 10; numero /=10;
+       centenas = numero % 10; numero /=10;
+       millares = numero % 10; numero /=10;
+
+       switch(millares)
+       {
+           case 1:
+               numeroRomano += "M";
+               break;
+           case 2:
+               numeroRomano += "MM";
+               break;
+
+           case 3:
+               numeroRomano += "MMM";
+               break;
+
+       }
+
+       switch(centenas)
+       {
+           case 1:
+               numeroRomano += "C";
+               break;
+           case 2:
+               numeroRomano += "CC";
+               break;
+           case 3:
+               numeroRomano += "CCC";
+               break;
+           case 4:
+               numeroRomano += "CD";
+               break;
+           case 5:
+               numeroRomano += "D";
+               break;
+           case 6:
+               numeroRomano += "DC";
+               break;
+           case 7:
+               numeroRomano += "DCC";
+               break;
+           case 8:
+               numeroRomano += "DCC";
+               break;
+           case 9:
+               numeroRomano += "CM";
+               break;
+
+
+       }
+
+       switch(decenas)
+       {
+           case 1:
+               numeroRomano += "X";
+               break;
+           case 2:
+               numeroRomano += "XX";
+               break;
+           case 3:
+               numeroRomano += "XXX";
+               break;
+           case 4:
+               numeroRomano += "XL";
+               break;
+           case 5:
+               numeroRomano += "L";
+               break;
+           case 6:
+               numeroRomano += "LX";
+               break;
+           case 7:
+               numeroRomano += "LXX";
+               break;
+           case 8:
+               numeroRomano += "LXXX";
+               break;
+           case 9:
+               numeroRomano += "XC";
+               break;
+       }
+       switch(unidades)
+       {
+           case 1:
+               numeroRomano += "I";
+               break;
+           case 2:
+               numeroRomano += "II";
+               break;
+           case 3:
+               numeroRomano += "III";
+               break;
+           case 4:
+               numeroRomano += "IV";
+               break;
+           case 5:
+               numeroRomano += "V";
+               break;
+           case 6:
+               numeroRomano += "VI";
+               break;
+           case 7:
+               numeroRomano += "VII";
+               break;
+           case 8:
+               numeroRomano += "VIII";
+               break;
+           case 9:
+               numeroRomano += "IX";
+               break;
+       }
+
+
+    return numeroRomano;
+   }
+
+//EJERCICIO 8
+
+bool contrasena(string evaluar, string Maruchan)
+{
+  bool A;
+  if(evaluar == Maruchan)
+    A=true;
+  else
+    A=false;
+
+}
+
+
+//EJERCICIO 7
+
+   bool esPrimo(int numero)
+   {
+       bool aux;
+       if(numero <= 1)
+         aux = false;
+       else
+           aux = true;
+       for(int i = 2; i < numero; i++)
+       {
+           if ((numero & i) == 0)
+               aux = false;
+       }
+
+    return aux;
+   }
 
 //EJERCICIO 9
 
-/*
-void TablaDel(int n, int * Tabla)
+   void tablaDel(int n, int *tabla)
+    {
+        for(int i = 0; i < 10; ++i)
+            tabla[i] = n * (i+1);
 
-{
-    for(int i=0; i<10 ; ++i)
-      Tabla{i} = n * (i+1);
-}
 
-*/
+    }
+//EJERCICIO 10
 
-#endif // TP1_H_INCLUDED
+  void fibonacci(int n, int *sucesion)
+
+   {
+       long aux = 1, fib = 0;
+       for(int i = 1; i <= n; i++)
+       {
+           aux += fib;
+           fib = aux - fib;
+           sucesion[i - 1] = fib;
+       }
+   }
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,30 +1,195 @@
+/* ----------------------------------------------------------------------
+                   TP1 - Programacion de funciones en C++
+
+   Descripcion: Plantilla para el desarrollo del trabajo practico no. 1
+   Alumno: Zdasien Stefano
+   Curso: 6to 1ra (A)
+   Ciclo lectivo: 2022
+   ---------------------------------------------------------------------- */
+
 #include <iostream>
+#include <windows.h>
 #include "TP1.h"
-
 using namespace std;
+using std::string;
+/* La opcion para salir */
+#define SALIR 'S'
 
-//EJERCICIO 1
+/* ----------------------------------------------------------------------
+   Prototipos
+   ---------------------------------------------------------------------- */
+void menu(void);                        /* Presenta el menu */
+int leerOpcion(void);                   /* Lee la opcion del usuario */
+float superficieCirculo(int);           /* Calcula la superficie del circulo */
+long suma(int, int); 				/* Calcula la suma de numeros en un rango */
+long potencia(int, int);			     /* Calcula la potencia de un numero */
+long factorial(int);	               /* Calcula el factorial de un numero */
+int edad(int, int, int);				/* Calcula la edad de una persona */
+string romanos(int);				/* Convierte un numero en notacion romana */
+bool esPrimo(int);			  		/* Determina si un numero es primo o no */
+bool contrasena(string, string);        /* Determina si la contrasena ingresada es correcta*/
+void tablaDel(int, int *);  			/* Calcula y devuelve la tabla de multplicacion de un numero */
+void fibonacci(int, int *);		     /* Calcula y devuelve la sucesión de Fibonacci */
+/* ---------------------------------------------------------------------- */
+
+int leerOpcion(void)
+{
+     char opcion;
+
+     opcion = getchar();
+     while (opcion <= '0' || opcion > SALIR)
+        opcion = getchar();
+
+     return opcion;
+}
+
+void menu(void)
+{
+     system("CLS");
+     cout << "Menu:" << endl;
+     cout << "  1. Calcula la superficie de un circulo." << endl;
+     cout << "  2. Suma numeros consecutivos entre dos valores enteros." << endl;
+     cout << "  3. Calcula la potencia de un numero (x^y)." << endl;
+     cout << "  4. Calcula el factorial de un numero." << endl;
+     cout << "  5. Calcula la edad de una persona." << endl;
+     cout << "  6. Convierte un numero a notacion romana." << endl;
+     cout << "  7. Determina si un numero es primo." << endl;
+     cout << "  8. Determina si la contrasena ingresada es valida." << endl;
+     cout << "  9. Calcula la tabla de multplicacion de un numero." << endl;
+     cout << "  0. Calcula y muestra la sucesión de Fibonacci." << endl;
+     cout << "  S. Salir." << endl;
+     cout << "  " << endl;
+     cout << "  Ingrese la opcion: ";
+}
+
+
+
+
+
+
 int main()
 {
-    cout << "Superficie Circunferencia Trigonometrica = ";
-    cout << superficieCirculo(1) << endl;
-    return 0;
+    int radio, numero, desde, hasta, base, exponente, anio, mes, dia, tabla[10], sucesion[100];
+    string unaContrasena;
+    char opcion = 'x';
 
-    long potencia(int a, int b)
+    while (opcion != SALIR)
+    {
+     menu();
+     opcion = leerOpcion();
+     switch (opcion)
+          {
+          case '1':
+    		     system("CLS");
+    			cout << "Ej. 1 - Calcula la superficie del circulo." << endl;
+    			cout << "Ingrese el radio del circulo: ";
+		     cin >> radio;
+    			cout << "La superficie es: " << superficieCirculo(radio) << endl;
+               return 0;
+               break;
 
-    cout << "ingrese el denominador";
-    cin  >> a;
-    cout << "ingrese la potencia";
-    cin  >> b;
-    cout << "la respuesta es: " << a + b;
-    return 0;
 
+          case '2':
+               system("CLS");
+               cout << "Ej. 2 - Calcula la suma de numeros dentro desde un rango." << endl;
+               cout << "Ingrese el numero desde: ";
+               cin >> desde;
+               cout << "Ingrese el numero hasta: ";
+               cin >> hasta;
+               cout << "La suma entre " << desde << " y " << hasta << " es: " << suma(desde, hasta);
+               return 0;
+               break;
 
+          case '3':
+               system("CLS");
+               cout << "Ej. 3 - Calcula la potencia de un numero (x^y)." << endl;
+               cout << "Ingrese la base (x): ";
+               cin >> base;
+               cout << "Ingrese el exponente (y): ";
+               cin >> exponente;
+               cout << "La potencia x^y es: " << potencia(base, exponente);
+               return 0;
+               break;
 
+          case '4':
+               system("CLS");
+               cout << "Ej. 4 - Calcula el factorial de un numero." << endl;
+               cout << "Ingrese un numero: ";
+               cin >> numero;
+               cout << "El factorial de " << numero << " es " << factorial(numero);
+               return 0;
+               break;
 
+          case '5':
+            	system("CLS");
+            	cout << "Ej. 5 - Calcula la edad de una persona." << endl;
+            	cout << "Ingrese anio de nacimiento: ";
+            	cin >> anio;
+            	cout << "Ingrese mes de nacimiento: ";
+            	cin >> mes;
+			cout << "Ingrese dia de nacimiento: ";
+            	cin >> dia;
+            	cout << "La edad es: " << edad(dia, mes, anio);
+            	return 0;
+            	break;
 
+          case '6':
+            	system("CLS");
+            	cout << "Ej. 6 - Convierte un numero a notacion romana." << endl;
+            	cout << "Ingrese numero a convertir: ";
+            	cin >> numero;
+            	cout << "La notacion romana de " << numero << " es: " << romanos(numero);
+                return 0;
+            	break;
 
+          case '7':
+               system("CLS");
+               cout << "Ej. 7 - Determina si un numero es primo o no." << endl;
+               cout << "Ingrese un numero: ";
+               cin >> numero;
+               if (esPrimo(numero))
+                    cout << "El numero es primo." << endl;
+               else
+                    cout << "El numero no es primo." << endl;
+               return 0;
+               break;
 
+          case '8':
+               system("CLS");
+               cout << "Ej. 8 - Determina si la contrasena ingresada es correcta." << endl;
+               cout << "Ingrese contrasena: ";
+               cin >> unaContrasena;
+               if (contrasena(unaContrasena, "Maruchan"))
+                    cout << "Contrasena correcta";
+               else
+                    cout << "Contrasena incorrecta";
+               return 0;
+               break;
+
+          case '9':
+               system("CLS");
+               cout << "Ej. 9 - Calcula la tabla de multplicacion de un numero." << endl;
+               cout << "Ingrese un numero: ";
+               cin >> numero;
+               cout << "la " << numero << "es" <<tabla[10];
+               // agregar la logica para mostrar el contenido
+               return 0;
+               break;
+
+          case '0':
+               system("CLS");
+               cout << "Ej. 10 - Calcula y muestra la sucesion de Fibonacci." << endl;
+               cout << "Ingrese un numero: ";
+               cin >> numero;
+               fibonacci(numero, sucesion);
+               // agregar la logica para mostrar el contenido
+               return 0;
+               break;
+
+          }
+     }
+
+     return 0;
 
 
 
